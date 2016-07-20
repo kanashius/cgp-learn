71ed7b7829d858bdd5f6ab2524fc9096a436977e
Patrick Nolte <pnolte@uos.de>
2016-07-20 10:33:58 +0200

    Task 3.1 done

---
+++ task4/patrick_nolte/main.rs
@@ -0,0 +1,12 @@
+use std::ops::{Add, Mul};
+fn magic<T>(first: &T, second: &T) -> (T, T)
+    where T: Mul<Output = T> + Add<Output = T> + Copy
+{
+    (first.clone() + second.clone(), first.clone() * second.clone())
+}
+
+fn main() {
+    let a = 4;
+    let b = 3;
+    println!("{} +/* {}: {:?}", a, b, magic(&a, &b));
+}
